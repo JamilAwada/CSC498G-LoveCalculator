67c486550a0ef293c9a5b51444c113e1d3f085f3
JamilAwada <jamil.awada@lau.edu>
2022-02-17 01:45:51 +0200

    fixed bugs that led to crash

--- app/src/main/java/com/jamdev/geekslovecalculator/MainActivity.java
+++ app/src/main/java/com/jamdev/geekslovecalculator/MainActivity.java
@@ -53,37 +53,37 @@ public class MainActivity extends AppCompatActivity {
         if (fullName.isEmpty()){
             Toast.makeText(getApplicationContext(), "Empty field, please enter your name!", Toast.LENGTH_LONG).show();
         }
-        else { 
+        else {
             String selectedLanguage = sp.getSelectedItem().toString();
             int randomInt = getRandomInt();
             result.setText(randomInt + "%");
             if (selectedLanguage.equals("Java")){
                 icon.setImageResource(R.drawable.java);
-                YoYo.with(Techniques.SlideOutLeft).duration(99999999).repeat(0).playOn(icon);
+                YoYo.with(Techniques.SlideInRight).duration(700).repeat(0).playOn(icon);
             }
             else if (selectedLanguage.equals("Kotlin")){
                 icon.setImageResource(R.drawable.kotlin);
-                YoYo.with(Techniques.SlideOutLeft).duration(99999999).repeat(0).playOn(icon);
+                YoYo.with(Techniques.SlideInRight).duration(700).repeat(0).playOn(icon);
             }
             else if (selectedLanguage.equals("Python")){
                 icon.setImageResource(R.drawable.python);
-                YoYo.with(Techniques.SlideOutLeft).duration(99999999).repeat(0).playOn(icon);
+                YoYo.with(Techniques.SlideInRight).duration(700).repeat(0).playOn(icon);
             }
             else if (selectedLanguage.equals("C")){
                 icon.setImageResource(R.drawable.c);
-                YoYo.with(Techniques.SlideOutLeft).duration(99999999).repeat(0).playOn(icon);
+                YoYo.with(Techniques.SlideInRight).duration(700).repeat(0).playOn(icon);
             }
             else if (selectedLanguage.equals("C++")){
                 icon.setImageResource(R.drawable.c__);
-                YoYo.with(Techniques.SlideOutLeft).duration(99999999).repeat(0).playOn(icon);
+                YoYo.with(Techniques.SlideInRight).duration(700).repeat(0).playOn(icon);
             }
             else if (selectedLanguage.equals("C#")){
                 icon.setImageResource(R.drawable.c_);
-                YoYo.with(Techniques.SlideOutLeft).duration(99999999).repeat(0).playOn(icon);
+                YoYo.with(Techniques.SlideInRight).duration(700).repeat(0).playOn(icon);
             }
             else if (selectedLanguage.equals("JavaScript")){
                 icon.setImageResource(R.drawable.javascript);
-                YoYo.with(Techniques.SlideOutLeft).duration(99999999).repeat(0).playOn(icon);
+                YoYo.with(Techniques.SlideInRight).duration(700).repeat(0).playOn(icon);
             }
         }
     }
