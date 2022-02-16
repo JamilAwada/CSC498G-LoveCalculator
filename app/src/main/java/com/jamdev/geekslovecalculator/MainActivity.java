2f596b7c675f021fe9b4a5f68da5bf60944f7f92
JamilAwada <jamil.awada@lau.edu>
2022-02-17 01:33:38 +0200

    added main functionality and animation

--- app/src/main/java/com/jamdev/geekslovecalculator/MainActivity.java
+++ app/src/main/java/com/jamdev/geekslovecalculator/MainActivity.java
@@ -3,10 +3,25 @@ package com.jamdev.geekslovecalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Declaring the global variables
+    Spinner sp;
+    TextView result, name;
+    ImageView icon;
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +32,59 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
 
+        // Initializing the variables
+        sp = findViewById(R.id.spinner_language);
+        result = findViewById(R.id.text_number);
+        name = findViewById(R.id.input_fullname);
+        btn = findViewById(R.id.button);
+        icon = findViewById(R.id.img_icon);
+
+    }
+
+    public int getRandomInt(){
+        int min = 0;
+        int max = 100;
+        int randomInt = (int) Math.floor(Math.random() * (max - min + 1) + min);
+        return randomInt;
+    }
+
+    public void getResult(View view){
+        String fullName = name.getText().toString();
+        if (fullName.isEmpty()){
+            Toast.makeText(getApplicationContext(), "Empty field, please enter your name!", Toast.LENGTH_LONG).show();
+        }
+        else { 
+            String selectedLanguage = sp.getSelectedItem().toString();
+            int randomInt = getRandomInt();
+            result.setText(randomInt + "%");
+            if (selectedLanguage.equals("Java")){
+                icon.setImageResource(R.drawable.java);
+                YoYo.with(Techniques.SlideOutLeft).duration(99999999).repeat(0).playOn(icon);
+            }
+            else if (selectedLanguage.equals("Kotlin")){
+                icon.setImageResource(R.drawable.kotlin);
+                YoYo.with(Techniques.SlideOutLeft).duration(99999999).repeat(0).playOn(icon);
+            }
+            else if (selectedLanguage.equals("Python")){
+                icon.setImageResource(R.drawable.python);
+                YoYo.with(Techniques.SlideOutLeft).duration(99999999).repeat(0).playOn(icon);
+            }
+            else if (selectedLanguage.equals("C")){
+                icon.setImageResource(R.drawable.c);
+                YoYo.with(Techniques.SlideOutLeft).duration(99999999).repeat(0).playOn(icon);
+            }
+            else if (selectedLanguage.equals("C++")){
+                icon.setImageResource(R.drawable.c__);
+                YoYo.with(Techniques.SlideOutLeft).duration(99999999).repeat(0).playOn(icon);
+            }
+            else if (selectedLanguage.equals("C#")){
+                icon.setImageResource(R.drawable.c_);
+                YoYo.with(Techniques.SlideOutLeft).duration(99999999).repeat(0).playOn(icon);
+            }
+            else if (selectedLanguage.equals("JavaScript")){
+                icon.setImageResource(R.drawable.javascript);
+                YoYo.with(Techniques.SlideOutLeft).duration(99999999).repeat(0).playOn(icon);
+            }
+        }
     }
 }
