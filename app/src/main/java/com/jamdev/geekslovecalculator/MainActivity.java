92cd2476a8a1a63322bfcc60bfd78c7c14681a26
JamilAwada <jamil.awada@lau.edu>
2022-02-17 04:12:59 +0200

    added table

--- app/src/main/java/com/jamdev/geekslovecalculator/MainActivity.java
+++ app/src/main/java/com/jamdev/geekslovecalculator/MainActivity.java
@@ -2,18 +2,28 @@ package com.jamdev.geekslovecalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.daimajia.androidanimations.library.Techniques;
 import com.daimajia.androidanimations.library.YoYo;
+
+import org.w3c.dom.Text;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,6 +32,10 @@ public class MainActivity extends AppCompatActivity {
     TextView result, name;
     ImageView icon;
     Button btn;
+    TableLayout tbl;
+    ArrayList<String> languages = new ArrayList<String>();
+    ArrayList<String> scores = new ArrayList<String>();
+    int roundsPlayed = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +52,7 @@ public class MainActivity extends AppCompatActivity {
         name = findViewById(R.id.input_fullname);
         btn = findViewById(R.id.button);
         icon = findViewById(R.id.img_icon);
+        tbl = findViewById(R.id.table_main);
 
     }
 
@@ -48,6 +63,40 @@ public class MainActivity extends AppCompatActivity {
         return randomInt;
     }
 
+    public void insertRow(){
+        Typeface face = Typeface.create("casual", Typeface.NORMAL);
+        TableRow tempRow = new TableRow(this);
+
+        TextView languageCol = new TextView(this);
+        languageCol.setText(languages.get(roundsPlayed).toString());
+        languageCol.setTextSize(16);
+        languageCol.setGravity(Gravity.CENTER);
+        languageCol.setTypeface(face);
+        tempRow.addView(languageCol);
+
+        TextView scoreCol = new TextView(this);
+        scoreCol.setText((scores.get(roundsPlayed).toString()));
+        scoreCol.setTextSize(16);
+        scoreCol.setGravity(Gravity.CENTER);
+        scoreCol.setTypeface(face);
+        scoreCol.setTextColor(Color.RED);
+        tempRow.addView(scoreCol);
+
+        tbl.addView(tempRow);
+
+        roundsPlayed++;
+    }
+
+    public void clear(View view){
+        roundsPlayed = 0;
+        languages.clear();
+        scores.clear();
+        result.setText("");
+        for (int i = 1; i < 10; i++){
+            tbl.removeView(tbl.getChildAt(i));
+        }
+    }
+
     public void getResult(View view){
         String fullName = name.getText().toString();
         // Check if user input a name
@@ -56,45 +105,53 @@ public class MainActivity extends AppCompatActivity {
             YoYo.with(Techniques.Wobble).duration(700).repeat(0).playOn(name);
         }
         else {
-            String selectedLanguage = sp.getSelectedItem().toString();
-            int randomInt = getRandomInt();
-            result.setText(randomInt + "%");
-            if (selectedLanguage.equals("Java")){
-                icon.setImageResource(R.drawable.java);
-                YoYo.with(Techniques.SlideInRight).duration(700).repeat(0).playOn(icon);
-            }
-            else if (selectedLanguage.equals("Kotlin")){
-                icon.setImageResource(R.drawable.kotlin);
-                YoYo.with(Techniques.SlideInRight).duration(700).repeat(0).playOn(icon);
-            }
-            else if (selectedLanguage.equals("Python")){
-                icon.setImageResource(R.drawable.python);
-                YoYo.with(Techniques.SlideInRight).duration(700).repeat(0).playOn(icon);
-            }
-            else if (selectedLanguage.equals("C")){
-                icon.setImageResource(R.drawable.c);
-                YoYo.with(Techniques.SlideInRight).duration(700).repeat(0).playOn(icon);
-            }
-            else if (selectedLanguage.equals("C++")){
-                icon.setImageResource(R.drawable.c__);
-                YoYo.with(Techniques.SlideInRight).duration(700).repeat(0).playOn(icon);
-            }
-            else if (selectedLanguage.equals("C#")){
-                icon.setImageResource(R.drawable.c_);
-                YoYo.with(Techniques.SlideInRight).duration(700).repeat(0).playOn(icon);
-            }
-            else if (selectedLanguage.equals("JavaScript")) {
-                icon.setImageResource(R.drawable.javascript);
-                YoYo.with(Techniques.SlideInRight).duration(700).repeat(0).playOn(icon);
-            }
-            if (randomInt <= 33){
-                YoYo.with(Techniques.Wobble).duration(700).repeat(5).playOn(icon);
-            }
-            else if (randomInt > 33 && randomInt <= 66){
-                YoYo.with(Techniques.Bounce).duration(700).repeat(5).playOn(icon);
+            if (roundsPlayed < 9){
+                String selectedLanguage = sp.getSelectedItem().toString();
+                languages.add(selectedLanguage);
+                int randomInt = getRandomInt();
+                scores.add(Integer.toString(randomInt) + "%");
+                result.setText(randomInt + "%");
+                if (selectedLanguage.equals("Java")){
+                    icon.setImageResource(R.drawable.java);
+                    YoYo.with(Techniques.SlideInRight).duration(700).repeat(0).playOn(icon);
+                }
+                else if (selectedLanguage.equals("Kotlin")){
+                    icon.setImageResource(R.drawable.kotlin);
+                    YoYo.with(Techniques.SlideInRight).duration(700).repeat(0).playOn(icon);
+                }
+                else if (selectedLanguage.equals("Python")){
+                    icon.setImageResource(R.drawable.python);
+                    YoYo.with(Techniques.SlideInRight).duration(700).repeat(0).playOn(icon);
+                }
+                else if (selectedLanguage.equals("C")){
+                    icon.setImageResource(R.drawable.c);
+                    YoYo.with(Techniques.SlideInRight).duration(700).repeat(0).playOn(icon);
+                }
+                else if (selectedLanguage.equals("C++")){
+                    icon.setImageResource(R.drawable.c__);
+                    YoYo.with(Techniques.SlideInRight).duration(700).repeat(0).playOn(icon);
+                }
+                else if (selectedLanguage.equals("C#")){
+                    icon.setImageResource(R.drawable.c_);
+                    YoYo.with(Techniques.SlideInRight).duration(700).repeat(0).playOn(icon);
+                }
+                else if (selectedLanguage.equals("JavaScript")) {
+                    icon.setImageResource(R.drawable.javascript);
+                    YoYo.with(Techniques.SlideInRight).duration(700).repeat(0).playOn(icon);
+                }
+                if (randomInt <= 33){
+                    YoYo.with(Techniques.Wobble).duration(700).repeat(5).playOn(icon);
+                }
+                else if (randomInt > 33 && randomInt <= 66){
+                    YoYo.with(Techniques.Bounce).duration(700).repeat(5).playOn(icon);
+                }
+                else {
+                    YoYo.with(Techniques.Pulse).duration(700).repeat(5).playOn(icon);
+                }
+                insertRow();
             }
             else {
-                YoYo.with(Techniques.Pulse).duration(700).repeat(5).playOn(icon);
+                Toast.makeText(getApplicationContext(), "Too many rounds played, please clear the table!", Toast.LENGTH_LONG).show();
             }
         }
     }
